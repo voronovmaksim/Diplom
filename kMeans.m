9c3f67c8dff264167f0f1d5999b42d7fe77744e5
mavo <voronov.maksim.v@gmail.com>
2019-09-16 15:45:53 +0300

    divided by modul

---
+++ kMeans.m
@@ -0,0 +1,36 @@
+function [newCentroidsOut,indexOfDataIsClusterOut] = kMeans(data, centroids)
+    numOfClusters=length(centroids);
+
+    distance = abs(data-centroids');
+    [smallerDistance, indexOfDataIsCluster] = min (distance);
+
+    centroidNodeSum = zeros(1,numOfClusters);
+    for i=1:1:length(indexOfDataIsCluster)
+         centroidNodeSum(indexOfDataIsCluster(i)) = centroidNodeSum(indexOfDataIsCluster(i)) + data(i);
+    end
+
+    clusters = unique(indexOfDataIsCluster);
+    clusterNodeCount = zeros(1,length(clusters));
+    for j=1:1:length(clusters)
+        cluster = clusters(j);
+        clusterNodeCount(j) = sum(indexOfDataIsCluster == cluster);
+    end
+
+    newCentroids = fix(centroidNodeSum./clusterNodeCount);
+
+    isAchived = true;
+
+
+    if ~(isAchived && isequal(centroids, newCentroids))
+        isAchived=false;
+    end
+
+    if isAchived == false
+       [getCentroids,getindexOfDataIsCluster] = kMeans(data, newCentroids);
+       newCentroidsOut = getCentroids;
+       indexOfDataIsClusterOut = getindexOfDataIsCluster;
+    else
+        newCentroidsOut = centroids;
+        indexOfDataIsClusterOut = indexOfDataIsCluster;
+    end
+end    
--- main.m
+++ main.m
@@ -1,51 +1,5 @@
 data = [2 4 -10 12 3 20 30 9 11];
+centroids = [1 5 30];
 
-
-%function
-centroid = [1 5 30];
-numOfClusters=length(centroid);
-
-distance = abs(data-centroid');
-[smallerDistance, indexOfDataIsCluster] = min (distance);
-
-
-centroidNodeSum = zeros(1,numOfClusters);
-for i=1:1:length(indexOfDataIsCluster)
-     centroidNodeSum(indexOfDataIsCluster(i)) = centroidNodeSum(indexOfDataIsCluster(i)) + data(i);
-end
-
-clusters = unique(indexOfDataIsCluster);
-clusterNodeCount = zeros(1,length(clusters));
-for j=1:1:length(clusters)
-    cluster = clusters(j);
-    clusterNodeCount(j) = sum(indexOfDataIsCluster == cluster);
-end
-
-newCentroid = fix(centroidNodeSum./clusterNodeCount);
-
-isAchived = true;
-
-
-if ~(isAchived && isequal(centroid, newCentroid))
-    isAchived=false;
-end
-
-'Final result'
-'---------------------------------------------'
-if true%isAchived
-    for k=1:1:length(newCentroid)
-        valueCluster=[];
-        for q=1:1:length(data)
-            
-            if indexOfDataIsCluster(q)== k
-                 valueCluster(end+1) = data(q);
-            end
-        end
-        strcat(sprintf('clister %.f:',k),sprintf(' %d,',valueCluster))
-    end
- strcat('new centroid: ',sprintf('.%d',newCentroid))   
-else
-    %reqursia
-end
-    
+[newCentroids, indexOfDataCluster] = kMeans(data,centroids);
 
